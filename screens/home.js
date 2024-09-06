bd7bf599ec9d3b4b2772548215928a87217fd030
John Doe <facu.estanfor@gmail.com>
2024-09-06 19:57:50 -0300

    soy yo la machine

--- screens/home.js
+++ screens/home.js
@@ -1,13 +1,9 @@
 import React, { useState } from 'react';
 import { Text, StyleSheet, View, Image, TouchableOpacity } from 'react-native';
 import myImage from '../assets/fondo_home.png';
-import img1 from '../assets/img1.png';
-import img2 from '../assets/img2.png';
-import img4 from '../assets/img4.png';
-import img5 from '../assets/img5.png';
 
 
-export default function Home(props) {
+export default function Home() {
   return (
     <View style={styles.padre}>
       <View style={styles.billetera}>
@@ -21,22 +17,6 @@ export default function Home(props) {
       </View>
       <View style={styles.imagen}>
         <Image source={myImage} />
-      </View>
-      <View style={styles.barraInferior}>
-        <TouchableOpacity style={styles.botonBarra} onPress={() => props.navigation.navigate('Home') }>
-          <Image source={img1} style={styles.img1} />
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.botonBarra} onPress={() => props.navigation.navigate('Clases')}>
-          <Image source={img2} style={styles.img2} />
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.botonBarra} onPress={() => props.navigation.navigate('Clases')}>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.botonBarra} onPress={() => props.navigation.navigate('Notificaciones')}>
-        <Image source={img4} style={styles.img4} />
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.botonBarra5} onPress={() => props.navigation.navigate('User')}>
-        <Image source={img5} style={styles.img5} />
-        </TouchableOpacity>
       </View>
     </View>
   );
@@ -83,51 +63,5 @@ const styles = StyleSheet.create({
     fontSize: 16,
     paddingLeft: '10%',
     fontWeight: 'semibold',
-  },
-  barraInferior: {
-    position: 'absolute',
-    bottom: 0,
-    width: '100%',
-    height: 60,
-    backgroundColor: '#1e1e1e',
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    height: '8%',
-  },
-  botonBarra: {
-    width: '20%',
-    height: '80%',
-    justifyContent: 'center',
-    alignItems: 'center',
-    borderRightWidth: 1,
-    borderColor: '#847D7D',
-    marginTop: '2%',
-  },
-  botonBarra5: {
-    width: '20%',
-    height: '80%',
-    justifyContent: 'center',
-    alignItems: 'center',
-    marginTop: '1.5%',
-  },
-  img1:{
-    height:'65%',
-    width:'50%',
-    marginBottom:'20%',
-  },
-  img2:{
-    height: '50%',
-    width: '60%',
-    marginBottom: '20%',
-  },
-  img4:{
-    height: '65%',
-    width: '50%',
-    marginBottom: '25%',
-  },
-  img5:{
-    height: '65%',
-    width: '50%',
-    marginBottom: '23%',
   },
 });
